4c4ca51b18dbc04ffc479931f5152a30b43730cf
vflorrez1 <manuel1212@hotmail.co.uk>
2024-07-12 11:51:49 +0100

    test-2

--- RaspberryPi_JetsonNano/python/examples/vic-test-solo.py
+++ RaspberryPi_JetsonNano/python/examples/vic-test-solo.py
@@ -44,33 +44,8 @@ try:
     epd.displayPartBaseImage(epd.getbuffer(time_image))
     num = 0
     while (True):
-        # top rect
-        draw.rectangle([(0, y_top), (screen_width, y_mid)], outline=0)
-
-        # top first line
-        draw.line([(top_line_1, y_top), (top_line_1, y_mid)], fill=0, width=1)
-
-        # top second line
-        draw.line([(top_line_2, 0), (top_line_2, y_mid)], fill=0, width=1)
-
-        # bottom rect
-        draw.rectangle([(0, y_mid), (screen_width, y_bottom)], outline=0)
-
-        draw.line([(screen_width / 2, y_mid), (screen_width / 2, y_bottom)], fill=0, width=1)
-
-        # position
-        draw.text((15, top_half_line_height), 'P1', fill='black', font=font1)
-
-        # last lap
-        draw.text((70, top_half_line_height), '43.114', fill='black', font=font1)
-
-        # delta
-        draw.text((170, top_half_line_height), '0.455', fill='black', font=font1)
-
-        # best lap
-        draw.text((145, bottom_half_line_height), '43.115', fill='black', font=font1)
-
         # count down
+        draw.rectangle((120, 80, 220, 105), fill = 255)
         draw.text((17, bottom_half_line_height), time.strftime('%H:%M:%S'), fill='black', font=font1)
         epd.displayPartial(epd.getbuffer(time_image))
         num = num + 1
