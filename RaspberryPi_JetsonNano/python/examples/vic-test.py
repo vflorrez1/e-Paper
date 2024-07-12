4fd28646d3e918eb84f98ab2c73266b5ec68761e
vflorrez1 <manuel1212@hotmail.co.uk>
2024-07-12 10:41:15 +0100

    remove uneeded

---
+++ RaspberryPi_JetsonNano/python/examples/vic-test.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd2in13_V4
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    logging.info("epd2in13_V4 Demo")
+    
+    epd = epd2in13_V4.EPD()
+    logging.info("init and Clear")
+    epd.init()
+    epd.Clear(0xFF)
+
+    # Drawing on the image
+    font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
+    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+
+    # # partial update
+    logging.info("4.show time...")
+    time_image = Image.new('1', (epd.height, epd.width), 255)
+    time_draw = ImageDraw.Draw(time_image)
+    epd.displayPartBaseImage(epd.getbuffer(time_image))
+    num = 0
+    while (True):
+        time_draw.rectangle((120, 80, 220, 105), fill = 255)
+        time_draw.text((120, 80), time.strftime('%H:%M:%S'), font = font24, fill = 0)
+        epd.displayPartial(epd.getbuffer(time_image))
+        num = num + 1
+        if(num == 20):
+            break
+    
+    logging.info("Clear...")
+    epd.init()
+    epd.Clear(0xFF)
+    
+    logging.info("Goto Sleep...")
+    epd.sleep()
+        
+except IOError as e:
+    logging.info(e)
+    
+except KeyboardInterrupt:    
+    logging.info("ctrl + c:")
+    epd2in13_V4.epdconfig.module_exit(cleanup=True)
+    exit()
