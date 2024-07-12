79b4e9a34cb0b58b4fd58e54698a802a6c5e8a7c
vflorrez1 <manuel1212@hotmail.co.uk>
2024-07-12 10:47:49 +0100

    test 2

---
+++ RaspberryPi_JetsonNano/python/examples/vic-test-2.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd2in13_V4
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    logging.info("epd2in13_V4 Demo")
+    
+    epd = epd2in13_V4.EPD()
+    logging.info("init and Clear")
+    epd.init()
+    epd.Clear(0xFF)
+
+    # Drawing on the image
+    font1 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+
+    y_top, y_mid, y_top = 0, 50, 100
+
+    # # partial update
+    logging.info("Vics test time...")
+    time_image = Image.new('1', (epd.height, epd.width), 255)
+    draw = ImageDraw.Draw(time_image)
+    epd.displayPartBaseImage(epd.getbuffer(time_image))
+    num = 0
+    while (True):
+        draw.rectangle([(0, y_top), (200, y_mid)], outline=0)
+
+        # top first line
+        draw.line([(50, y_top), (50, y_mid)], fill = 0,width = 1)
+
+        # top second line
+        draw.line([(130, 0), (130, 50)], fill = 0,width = 1)
+
+        draw.rectangle([(0, 50), (200, 100)], outline=0)
+
+        draw.line([(100, 50),(100, 100)], fill = 0,width = 1)
+
+        # position
+        draw.text((15, 18), 'P1', fill='black', font=font1)
+
+        # last lap
+        draw.text((62, 18), '43.114', fill='black', font=font1)
+
+        # delta
+        draw.text((143, 18), '0.455', fill='black', font=font1)
+
+        # best lap
+        draw.text((125, 65), '43.115', fill='black', font=font1)
+
+        # count down
+        draw.text((11, 65), time.strftime('%H:%M:%S'), fill='black', font=font1)
+        epd.displayPartial(epd.getbuffer(time_image))
+        num = num + 1
+        if(num == 20):
+            break
+    
+    logging.info("Clear...")
+    epd.init()
+    epd.Clear(0xFF)
+    
+    logging.info("Goto Sleep...")
+    epd.sleep()
+        
+except IOError as e:
+    logging.info(e)
+    
+except KeyboardInterrupt:    
+    logging.info("ctrl + c:")
+    epd2in13_V4.epdconfig.module_exit(cleanup=True)
+    exit()
