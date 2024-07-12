3013486679916171762b76f6a05dd1dcc21a5bc0
vic <manuel1212@hotmail.co.uk>
2024-07-12 15:05:41 +0100

    another

--- RaspberryPi_JetsonNano/python/examples/vic-test.py
+++ RaspberryPi_JetsonNano/python/examples/vic-test.py
@@ -119,9 +119,6 @@ async def connect():
                         # count down
                         draw.text((17, bottom_half_line_height), racer_data['sessionCountDown'], fill='black', font=font1)
                         epd.displayPartial(epd.getbuffer(time_image))
-                        num = num + 1
-                        if (num == 20):
-                            break
                 except websockets.exceptions.ConnectionClosed:
                     print("Disconnected from the server")
                     break
